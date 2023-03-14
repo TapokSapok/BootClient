3b0e773421323363e3b769424fb3fa0bd25efa67
Roman <lerom2008@yandex.ru>
2023-03-14 08:50:19 +0500

    lst

--- bot/extends/trading/stopTrading.js
+++ bot/extends/trading/stopTrading.js
@@ -1,9 +1,11 @@
 module.exports = (el) => {
+
    el.style.background = '';
    el.style.color = '';
    el.style.fontWeight = '';
    el.textContent = 'Старт';
 
+   // if (!bots[i].activeTrading) return;
 
    for (let i = 0; i < bots.length; i++) {
       if (el.dataset.useBot === bots[i].username) {
